4ab9019ae7a20c0906ddb66c3714be0084dc40ce
Sharper Dev <guilherme.senadeaguiar626@gmail.com>
2026-06-01 22:06:25 -0300

    UTF8 BMFont support

--- romfs/scripts/core/screens/main_bottom_screen.lua
+++ romfs/scripts/core/screens/main_bottom_screen.lua
@@ -4,6 +4,7 @@ local SHCanvas = require("sh_canvas")
 local SHCBuilder = require("sh_canvas.shc_builder")
 local SHCDebugger = require("sh_canvas.shc_debugger")
 local SHCFonts = require("sh_canvas.shc_fonts")
+local utf8 = require("utf8")
 
 local canvasBottom = SHCanvas:new(BOTTOM_SCREEN)
 
@@ -12,24 +13,23 @@ function MainBottomScreen.setup()
 end
 
 function MainBottomScreen.update()
-    --debugText:setContent(SHCDebugger.getDebugContent())
     canvasBottom:draw()
-    local text = "abc"
+    local text = "abé"
     Graphics.initBlend(BOTTOM_SCREEN)
-    local position = { x = 150, y = 100 }
+    local position = { x = 100, y = 100 }
     local cursor = { x = position.x, y = position.y }
-    
-    for i = 1, #text do
-        local charCode = string.byte(text, i)
-        local charInfo = SHCFonts.getBMFont().data.chars[charCode]
-        
-        cursor.x = cursor.x + charInfo.xoffset
-        cursor.y = cursor.y + charInfo.yoffset
-        
-        Graphics.drawPartialImage(cursor.x, cursor.y, charInfo.x, charInfo.y, charInfo.width, charInfo.height,
-            SHCFonts.getBMFont().sheet)
-        cursor.x = cursor.x + charInfo.xadvance
-        cursor.y = position.y
+
+    for _, code in utf8.codes(text) do
+        local charInfo = SHCFonts.getBMFont().data.chars[code]
+        if charInfo then
+            cursor.x = cursor.x + charInfo.xoffset
+            cursor.y = cursor.y + charInfo.yoffset
+            
+            Graphics.drawPartialImage(cursor.x, cursor.y, charInfo.x, charInfo.y, charInfo.width, charInfo.height,
+                SHCFonts.getBMFont().sheet)
+            cursor.x = cursor.x + charInfo.xadvance
+            cursor.y = position.y
+        end
     end
     Graphics.termBlend()
 end
